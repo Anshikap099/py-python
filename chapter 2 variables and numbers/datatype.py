6e38148a839be4a1c50c2d77f761d9f2e66517c9
Anshika Pandey <anship.2006@gmial.com>
2026-06-24 19:42:16 +0530

    done

--- chapter 2 variables and numbers/datatype.py
+++ chapter 2 variables and numbers/datatype.py
@@ -3,3 +3,4 @@ b=2 # b is a floating point number
 c="anshika" # c is a string
 d= False #d is an boolean algebra
 e= None # e is a none type variable means kisi paarticulr variable mai kuch nhi hai
+# ghhju
