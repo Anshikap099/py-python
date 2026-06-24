76b767d4f1da042089aa2d503160596d59920a95
Anshika Pandey <anship.2006@gmial.com>
2026-06-24 09:48:42 +0530

    loops file

---
+++ chapter2 problemsolving/strings chapter 3/chapter3 practiceset/chapter4/chapter4practice set/dictonaryandsets,,,,,,,chapter=5/chapter 5 practce set/chapter6/chapter7/for nloop iterate.py
@@ -0,0 +1,14 @@
+#for looop with lists
+l=[1,2,33,2,6,45,6]
+for i in l:
+    print (i)
+
+#for loop with tuples
+t=(6,345,56,667)
+for i in t:
+        print(i)
+
+#for loop with strings
+s="anshika"
+for i in s:
+    print(i)
