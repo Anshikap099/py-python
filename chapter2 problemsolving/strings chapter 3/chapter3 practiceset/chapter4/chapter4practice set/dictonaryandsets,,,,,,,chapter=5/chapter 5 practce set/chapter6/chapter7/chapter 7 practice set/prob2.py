48424c089997283974365da0c0d861d7d7f90c17
Anshika Pandey <anship.2006@gmial.com>
2026-06-26 20:18:26 +0530

    changes

---
+++ chapter2 problemsolving/strings chapter 3/chapter3 practiceset/chapter4/chapter4practice set/dictonaryandsets,,,,,,,chapter=5/chapter 5 practce set/chapter6/chapter7/chapter 7 practice set/prob2.py
@@ -0,0 +1,5 @@
+#wap a programme to get all the person names stored in a list which starts with l
+l=["harry","soham","sachin","ram"]
+for name in l:
+    if (name.startswith("s")):
+        print(f"hello{name}")
