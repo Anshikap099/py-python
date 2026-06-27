45f563c0d725c0cb73b9c4a1a9cfadd5d7e830f1
Anshika Pandey <anship.2006@gmial.com>
2026-06-27 20:50:59 +0530

    ok

---
+++ chapter2 problemsolving/strings chapter 3/chapter3 practiceset/chapter4/chapter4practice set/dictonaryandsets,,,,,,,chapter=5/chapter 5 practce set/chapter6/chapter7/chapter 7 practice set/prob4.py
@@ -0,0 +1,7 @@
+# wrte a programme to finnd wether the number is even or odd
+n=int(input("enter a number:"))
+for i in range(1,n):
+    if (n%i)==0:
+        print("number is not prime")
+else:
+    print("number is not prime")
