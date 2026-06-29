dc618bf70f9066dcf4ebf783f0d8bd10411e5873
Anshika Pandey <anship.2006@gmial.com>
2026-06-29 08:49:57 +0530

    done

---
+++ chapter2 problemsolving/strings chapter 3/chapter3 practiceset/chapter4/chapter4practice set/dictonaryandsets,,,,,,,chapter=5/chapter 5 practce set/chapter6/chapter7/chapter 7 practice set/chapter 8 k/function.py
@@ -0,0 +1,22 @@
+# a function is a group of statement perfefrming a specific task.
+#its syntax is 
+# def func()
+#     print('haya')
+a=2
+b=4
+c=6
+# average=(a+b+c)/3
+# print(average)
+
+# a=int(input("enter your number "))
+# b=int(input("enter your number "))
+# c=int(input("enter your number "))
+# average=(a+b+c)/3
+# print(average)
+def avg():
+ a=int(input("enter your number "))
+b=int(input("enter your number "))
+c=int(input("enter your number "))
+
+average=(a+b+c)/3
+print(average)
